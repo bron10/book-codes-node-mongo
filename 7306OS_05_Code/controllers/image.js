c162eee0e9d792094996da16be4641ef467e01d4
Bruno Dmello <bron1010@gmail.com>
2017-08-09 09:20:37 +0530

    commit2 : Chapter 4,5,6,7,9,11,12 code shared

---
+++ 7306OS_05_Code/controllers/image.js
@@ -0,0 +1,14 @@
+module.exports = {
+    index(req, res){
+        res.render('image');
+    },
+    create(req, res){
+        res.send('The image:create POST controller');
+    },
+    like(req, res){
+        res.send('The image:like POST controller');
+    },
+    comment(req, res){
+        res.send('The image:comment POST controller');
+    }
+};
